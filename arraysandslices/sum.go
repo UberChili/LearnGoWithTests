4d4357be7d38488dfbc163831a01255629b2e397
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2024-12-18 18:31:32 -0600

    finished arrays and slices chapter

--- arraysandslices/sum.go
+++ arraysandslices/sum.go
@@ -9,7 +9,26 @@ func Sum(nums []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lengthofNumbers := len(numbersToSum)
-	sums := make([]int, lengthofNumbers)
-	return nil
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }

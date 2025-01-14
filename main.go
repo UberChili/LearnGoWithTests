9f77d8a84444e743e9c3ea2f02e3763523bad85d
Andrés <nunezlopez.ja@gmail.com>
2025-01-14 17:06:33 -0600

    Finished(?) Chapter Mocking. What a fucking mess I don't understand fuckall

--- main.go
+++ main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
+	"os"
+	"time"
+
+	"example.com/hello/mocking"
 )
 
 // This main is following the Dependency Injection chapter
 // At first I was importing the module from dependencyinjection module/directory
-
 // func Greet(writer io.Writer, name string) {
-func Greet(writer io.Writer, name string) {
-	fmt.Fprintf(writer, "Hello, %s", name)
-}
+// 	fmt.Fprintf(writer, "Hello, %s", name)
+// }
 
-func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
-	Greet(w, "World")
-}
+// func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+// 	Greet(w, "World")
+// }
 
+// func main() {
+// 	// Greet(os.Stdout, "Andrew")
+// 	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreetHandler)))
+// }
+
+// Next stuff is for the Mocking chapter...
+// Using the importing approach to uhm,... it feels more orderly I guess
 func main() {
-	// Greet(os.Stdout, "Andrew")
-	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreetHandler)))
+	sleeper := &mocking.ConfigurableSleeper{1 * time.Second, time.Sleep}
+	mocking.Countdown(os.Stdout, sleeper)
 }
--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -2,22 +2,57 @@ package mocking
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
-
-
 // Write the fucking test first godamit
-
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
 
-	Countdown(buffer)
+		got := buffer.String()
 
-	got := buffer.String()
-	want := "3"
+		want := `3
+2
+1
+Go!`
 
-	if got != want {
-		t.Errorf("Got %q, want %q", got, want)
+		if got != want {
+			t.Errorf("Got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v, got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 	}
 }
